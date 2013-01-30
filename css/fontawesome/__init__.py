7f90c2305836e216aacbf57894aabf297f1ed680
Jason Kölker <jason@koelker.net>
2013-01-30 10:29:22 -0600

    Fix css file name

--- css/fontawesome/__init__.py
+++ css/fontawesome/__init__.py
@@ -6,5 +6,5 @@ library = fanstatic.Library("fontawesome", "resources")
 fontawesome = fanstatic.Resource(library, "css/font-awesome.css",
                                  minified="css/font-awesome.min.css",
                                  depends=[bootstrap])
-fontawesome_ie7 = fanstatic.Resource(library, "css/font-awesome-ie7.css",
+fontawesome_ie7 = fanstatic.Resource(library, "css/font-awesome-ie7.min.css",
                                      depends=[bootstrap])
