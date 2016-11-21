0a31d45ec7e1ee3f2eb2cc93884a097a0d72afed
Heberte Fernandes de Moraes <brebete@gmail.com>
2016-11-21 14:06:03 -0200

    Updated fontawesome to version 4.7.0 Using classes with 'fa' prefixes like original version

--- css/fontawesome/__init__.py
+++ css/fontawesome/__init__.py
@@ -8,8 +8,3 @@ fontawesome = fanstatic.Resource(
     minified="css/font-awesome.min.css",
     depends=[bootstrap],
 )
-fontawesome_ie7 = fanstatic.Resource(
-    library, "css/font-awesome-ie7.css",
-    minified="css/font-awesome-ie7.min.css",
-    depends=[bootstrap],
-)
